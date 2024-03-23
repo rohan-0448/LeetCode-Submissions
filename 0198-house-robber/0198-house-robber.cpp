f0d90b7001611304533fd789e01cc7756a0bb73f
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-03-23 18:21:04 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (79.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-
-    static int check(vector<int> &dp, vector<int> &nums, int index){
+    
+    int check(vector<int>& nums, vector<int> &dp, int index){
         if(index==0) return nums[0];
         if(index<0) return 0;
+        
         if(dp[index]!=-1) return dp[index];
-
-        int take = INT_MIN, not_take = INT_MIN;
-        take = nums[index] + check(dp, nums, index-2);
-        not_take = check(dp, nums, index-1);
-
+        
+        int take = nums[index] + check(nums, dp, index-2);
+        int not_take = check(nums, dp, index-1);
+        
         return dp[index] = max(take, not_take);
     }
-
+    
     int rob(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return max(nums[0], nums[1]);
-        vector<int> dp(nums.size(),-1);
-        int x = max(check(dp, nums, nums.size()-1), check(dp, nums, nums.size()-2));
-        return dp[nums.size()-1];
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        
+        return check(nums, dp, n-1);
     }
 };
