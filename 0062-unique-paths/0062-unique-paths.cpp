aabbc0ec6de40cd7f0c9170b517f38eceac3c0a8
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-03-24 22:38:18 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (17.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,18 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m, vector<int> (n, 1));
+        dp[0][0] = 1;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                int left = 0, top = 0;
+                if(i > 0) top = dp[i-1][j];                
+                if(j > 0) left = dp[i][j-1];
+
+                dp[i][j] = top + left;
+            }
+        }
+        return dp[m-1][n-1];
         return f(m-1, n-1, dp);
     }
 };
