1a6e14e6ec8073092410a9eec887e3e40b17d94e
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-03-24 22:45:08 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (45.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // recusion with dp format
     int f(int x, int y, vector<vector<int>> &dp){
         if(x == 0 && y == 0) return 1;
         if(dp[x][y] != -1) return dp[x][y];
@@ -13,13 +14,14 @@ public:
     }
     
     int uniquePaths(int m, int n) {
+        // tabulation format
         vector<vector<int>> dp(m, vector<int> (n, 1));
         dp[0][0] = 1;
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 int left = 0, top = 0;
-                if(i > 0) top = dp[i-1][j];                
-                if(j > 0) left = dp[i][j-1];
+                if(i != 0) top = dp[i-1][j];                
+                if(j != 0) left = dp[i][j-1];
 
                 dp[i][j] = top + left;
             }
