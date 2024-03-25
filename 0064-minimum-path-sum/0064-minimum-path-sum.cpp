eb79ff339194e64e0bc3bf1c895c36baf43d2f91
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-03-25 23:49:19 +0530

    Time: 7 ms (59.64%), Space: 12.7 MB (13.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // Recursion with Memoization
     int f(vector<vector<int>> &dp, vector<vector<int>> &grid, int x, int y){
         if(x == 0 && y == 0) return grid[0][0];
         if(dp[x][y] != -1) return dp[x][y];
@@ -14,7 +15,23 @@ public:
     
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m,   vector<int> (n, 0));
+        
+        // Tabulation Method
+        for(int x = 0; x < m; x++){
+            for(int y = 0; y < n; y++){
+                int top = grid[x][y], left = grid[x][y];
+                if(x == 0 && y == 0) dp[x][y] = grid[x][y];
+                else{
+                    if(x > 0) top += dp[x-1][y];
+                    else top = INT_MAX;
+                    if(y > 0) left += dp[x][y-1];
+                    else left = INT_MAX;
+                }
+                dp[x][y] = min(top, left);
+            }
+        }
+        return dp[m-1][n-1];
         return f(dp, grid, m-1, n-1);
     }
 };
