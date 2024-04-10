3c706f4e9e3304a4dc674ab25c0853c770755c3f
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-04-10 16:25:13 +0530

    Time: 7 ms (18.53%), Space: 10.7 MB (91.52%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
 
-void print_queue(queue<int> q)
-{
-    queue<int> temp = q;
-    while (!temp.empty()) {
-        cout << temp.front()<<" ";
-        temp.pop();
-    }
-    cout << '\n';
-}
-
+// void print_queue(queue<int> q){
+//     queue<int> temp = q;
+//     while (!temp.empty()) {
+//         cout << temp.front()<<" ";
+//         temp.pop();
+//     }
+//     cout << '\n';
+// }
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         sort(deck.begin(), deck.end());
         int n = deck.size(), index = 0;
