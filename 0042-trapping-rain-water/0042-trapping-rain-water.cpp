3cf1c7d780e6f1ac7acdc3f98acaeb24368129ba
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-04-12 23:27:50 +0530

    Time: 17 ms (13.23%), Space: 22.4 MB (46.83%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        // Sliding Window Method
+        int n = height.size();
+        int left = 0, right = n-1, leftmax = 0, rightmax = 0, res = 0;
+        
+        while(left <= right) {
+            if(height[left] <= height[right]){
+                if(height[left] >= leftmax) leftmax = height[left++];
+                else res += leftmax - height[left++];
+            }else{
+                if(height[right] >= rightmax) rightmax = height[right--];
+                else res += rightmax - height[right--];
+            }
+        }
+        return res;
+    }
+};
