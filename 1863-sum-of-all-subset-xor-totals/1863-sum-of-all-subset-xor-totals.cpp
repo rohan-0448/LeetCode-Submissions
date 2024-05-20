458e6af06bf454853ba93efc766ea751fa48df4f
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-05-20 21:48:42 +0530

    Time: 3 ms (68.97%), Space: 8.4 MB (65.34%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int subsetXORSum(vector<int>& nums){
+        int n = nums.size();
+        int numOfSubsets = pow(2, n), result = 0;
+        
+        // binary representation of subsets help us identify whether to take an element of num or not
+        for (int subset = 1; subset < numOfSubsets; ++subset){
+            int runningXor = 0;
+            for (int i = 0, bits = subset; i < nums.size(); ++i, bits >>= 1) if (bits & 1) runningXor ^= nums[i];
+            result += runningXor;
+        }
+        
+        return result;
+    }
+};
