7407a331e0efa2c5067fe2353e3edc93a4050d0c
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-06-11 00:44:46 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (44.04%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int c = 0;
+        vector<int> temp = heights;
+        sort(heights.begin(), heights.end());
+        for(int i=0; i<heights.size(); i++) if(heights[i] != temp[i]) c++;
+        return c;
+    }
+};
