882b200e3e0123e1313e23f73ba5411821976a25
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-06-12 20:26:43 +0530

    Time: 36 ms (12.76%), Space: 94.5 MB (58.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-        sort(nums1.begin(), nums1.end());
-        int n = nums1.size();
-        if(n%2==0) return ((double)nums1[n/2]+(double)nums1[(n-1)/2])/2;
-        else return nums1[n/2];
+        int count = 0, total = nums1.size()+nums2.size();
+        double median = 0.0;
+        
+        int index1=0, index2=0;
+        if(total%2 == 1) {
+            while(count < total/2){
+                if(index1 < nums1.size() && (index2 >= nums2.size() || nums1[index1] < nums2[index2])) index1++;
+                else index2++;
+                
+                count++;
+            }
+            if (index1 < nums1.size() && (index2 >= nums2.size() || nums1[index1] < nums2[index2])) median = nums1[index1];
+            else median = nums2[index2];
+        }
+        else{
+            while(count < total/2 - 1){
+                if(index1 < nums1.size() && (index2 >= nums2.size() || nums1[index1] < nums2[index2])) index1++;
+                else index2++;
+                
+                count++;
+            }
+            int nextVal1 = 0, nextVal2 = 0;
+            if (index1 < nums1.size() && (index2 >= nums2.size() || nums1[index1] < nums2[index2])) nextVal1 = nums1[index1++];
+            else nextVal1 = nums2[index2++];
 
-        // vector<double> add;
-        // int i=0, j=0;
-        // int n = (nums1.size() + nums2.size())/2;
+            if (index1 < nums1.size() && (index2 >= nums2.size() || nums1[index1] < nums2[index2])) nextVal2 = nums1[index1];
+            else nextVal2 = nums2[index2];
 
-        // while(i<nums1.size() && j<nums2.size()){
-        //     if(nums1[i] < nums2[j]){
-        //         add.push_back(nums1[i++]);
-        //     }else{
-        //         add.push_back(nums2[j++]);
-        //     }
-        //     cout << i << " " << j << " " << n << endl;
-        //     if(add.size()-1==n || n==0){
-        //         if(n%2==0) return (add[n]+add[n-1])/2;
-        //         else return add[n];
-        //     }
-        // }
-        // while(i<nums1.size()) {
-        //     add.push_back(nums1[i++]);
-        //     if(add.size()-1==n || n==0){
-        //         if(n%2==0) return (add[n]+add[n-1])/2;
-        //         else return add[n];
-        //     }
-        // }
-        // while(j<nums2.size()) {
-        //     add.push_back(nums2[j++]);
-        //     if(add.size()-1==n || n==0){
-        //         if(n%2==0) return (add[n]+add[n-1])/2;
-        //         else return add[n];
-        //     }
-        // }
-
-        // if(add.size()-1==n || n==0){
-        //     if(n%2==0) return (add[n]+add[n-1])/2;
-        //     else return add[n];
-        // }
-        // return -1;
+            median = (nextVal1 + nextVal2) / 2.0;
+        }
+        return median;
     }
 };
