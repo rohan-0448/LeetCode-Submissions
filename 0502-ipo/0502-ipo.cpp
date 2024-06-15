9df4072133f1ff42a26d9aa0337f59cfc92e6c1c
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-06-16 02:18:53 +0530

    Time: 136 ms (75.56%), Space: 85.5 MB (42.66%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size(), index = 0;
+        vector<pair<int, int>> projects;
+        for (int i = 0; i < n; ++i) projects.emplace_back(capital[i], profits[i]);
+
+        sort(projects.begin(), projects.end());
+        priority_queue<int> maxHeap;
+
+        for (int j = 0; j < k; ++j) {
+            while (index < n && projects[index].first <= w) maxHeap.push(projects[index++].second);
+            if (maxHeap.empty()) break;
+
+            w += maxHeap.top();
+            maxHeap.pop();
+        }
+
+        return w;
+    }
+};
