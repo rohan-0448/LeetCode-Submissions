040b09cf487a779e9e6656210801d92270b8773c
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-07-06 23:35:48 +0530

    Time: 14 ms (22.73%), Space: 7.2 MB (75.98%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        set<int> s;
-        int total = 0;
-        while(s.count(n) == 0) {
-            if(n == 1) return true;
-            s.insert(n);
-            total = 0;
-            while(n) {
-                total += (n % 10) * (n % 10);
+        int c = 0;
+        while(n != 1 && n != 10 && n != 13 && n != 68){
+            int sum = 0;
+            while(n != 0){
+                sum += (n%10)*(n%10);
                 n /= 10;
             }
-            n = total;
+            cout << sum << endl;
+            n = sum;
+            c++;
+            if(c > 100) return false;
         }
-        return false;
+        return true;
     }
 };
