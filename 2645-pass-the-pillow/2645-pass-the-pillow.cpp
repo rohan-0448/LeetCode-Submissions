fb526a51deaabab38147778fc57f8671ca87d2f0
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-07-06 09:45:28 +0530

    Time: 3 ms (16.54%), Space: 7.2 MB (30.58%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
         if(n > time) return time+1;
-        if((time/(n-1)) % 2 == 1){
-            return n-(time%(n-1));
-        }
-        else{
-            return time%(n-1) + 1;
-        }
+        if((time/(n-1)) % 2 == 1)  return n-(time%(n-1));
+        else return time%(n-1) + 1;
         return -1;
     }
 };
