33e2692652c3b97f43bdd6b1558d5d457bf98dd0
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-07-08 12:09:47 +0530

    Time: 20 ms (27.49%), Space: 10.3 MB (87.12%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int n) {
+        if(n==1) return s;
+        string ans = "";
+        int m = s.length();
+        int in = (n-1)*2;
+        for(int i=0; i<m; i+=in) ans += s[i];
+
+        cout << ans << endl;
+
+        for(int i=1; i<n-1; i++){
+            int temp = i;
+            int c = (n-i-1)*2;
+            while(temp<m){
+                ans+=s[temp];
+                if(temp+c<m) ans+=s[temp+c];
+                temp+=in;
+            }
+            cout << ans << endl;
+        }
+        for(int i=n-1; i<m; i+=in) ans += s[i];
+        
+        return ans;
+    }
+};
