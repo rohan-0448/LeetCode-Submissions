fad81e01998b75bbb2ac9d0bf5cad6836376e3d3
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-07-08 14:19:45 +0530

    Time: 0 ms (100%), Space: 8.2 MB (26.84%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        if(nums.size()==0) return {};
-        
-        int s=0,e=0;
-        string str="";
+        if(nums.size() == 0) return {};
+
+        int s = 0, e = 0;
         vector<string> ans;
-        while(e<nums.size()-1){
-            if(nums[e]+1==nums[e+1]) e++;
+
+        while(e< nums.size()-1){
+            if(nums[e+1] == nums[e] + 1) e++;
             else{
-                if(s!=e) str=to_string(nums[s])+"->"+to_string(nums[e]);
-                else str=to_string(nums[e]);
-                ans.push_back(str);
-                s=e+1;
-                e=s;
+                if(s==e) ans.push_back(to_string(nums[s]));
+                else ans.push_back(to_string(nums[s]) + "->" + to_string(nums[e]));
+
+                e++;
+                s = e;
             }
-        }
-        if(s!=e) str=to_string(nums[s])+"->"+to_string(nums[e]);
-        else str=to_string(nums[e]);
-        
-        ans.push_back(str);
+        } 
+
+        if(s==e) ans.push_back(to_string(nums[s]));
+        else ans.push_back(to_string(nums[s]) + "->" + to_string(nums[e]));
+
         return ans;
     }
 };
