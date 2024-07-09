59b802392a3ac75f7a516df5b1324810d7436b1f
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-07-09 17:39:42 +0530

    Time: 49 ms (38.4%), Space: 50.9 MB (51.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i=0;
-        for(int a=0; a<nums.size(); a++){
-            if(a>i) return false;
-            i = max(i, a+nums[a]);
+        int mx = 0, n = nums.size();
+        for(int i=0; i<n; i++){
+            if(i > mx) return false;
+            if(mx == n-1) return true;
+
+            mx = max(mx, i+nums[i]);
         }
         return true;
     }
