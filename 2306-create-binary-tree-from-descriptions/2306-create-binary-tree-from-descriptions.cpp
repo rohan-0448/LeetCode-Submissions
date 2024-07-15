b3a3d43e04839e5c2bcbf313054e3fa57c71661b
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-07-15 22:57:12 +0530

    Time: 710 ms (53.28%), Space: 289.4 MB (42.8%) - LeetHub

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& des) {
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> children;
+
+        for(auto i: des){
+            int parent = i[0], child = i[1], isLeft = i[2];
+
+            if(mp.find(parent) == mp.end()) mp[parent] = new TreeNode(parent);
+            if(mp.find(child) == mp.end()) mp[child] = new TreeNode(child);
+
+            if(isLeft == 1) mp[parent]->left = mp[child];
+            else mp[parent]->right = mp[child];
+
+            children.insert(child);
+        }
+
+        TreeNode* root = nullptr;
+        for(auto& node : mp) {
+            if(children.find(node.first) == children.end()) {
+                root = node.second;
+                break;
+            }
+        }
+
+        return root;
+    }
+};
