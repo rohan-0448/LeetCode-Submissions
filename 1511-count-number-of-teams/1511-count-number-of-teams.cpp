c3dbbaa3715e1ba594f3fdd92f40491b833f6410
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-08-01 11:19:26 +0530

    Time: 25 ms (72.08%), Space: 11.7 MB (68.86%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static int  numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int cnt= 0;
+        for(int i=1; i<n-1; i++) {
+            int left[2]={0}, right[2]={0};
+
+            for(int j = 0; j<i; j++) left[rating[j] < rating[i]]++;
+            for(int k=i+1; k<n; k++) right[rating[k] < rating[i]]++;
+
+            cnt += left[0]*right[1] + left[1]*right[0];
+        }
+        return cnt;
+    }
+};
