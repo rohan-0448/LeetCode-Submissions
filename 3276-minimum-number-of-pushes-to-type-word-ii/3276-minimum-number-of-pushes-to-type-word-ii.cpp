e398837f8de7fdbc3e1c945e75e636b18bcaca7a
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-08-06 14:06:59 +0530

    Time: 126 ms (21.86%), Space: 27.4 MB (31.85%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,33 +2,19 @@ class Solution {
 public:
     int minimumPushes(string word) {
         unordered_map<char, int> mp;
-        // int curr = 1, count = 1, total = 0;
-        for(auto i: word){
-            mp[i]++;
-            // if(mp.find(i) == mp.end()) {
-            //     mp[i] = curr;
-            //     count++;
-            // }
-            // if(count == 8){
-            //     count = 1;
-            //     curr++;
-            // }
-            // total+=mp[i];
-        }
+        for(auto i: word) mp[i]++;
+
         priority_queue<pair<int, char>> pq;
         for(auto i: mp) pq.push({i.second, i.first});
 
-        int total = 0;
-        int curr = 1;
+        int total = 0, curr = 1;
         while(!pq.empty()){
             int tmp = 8;
             while(!pq.empty() && tmp--){
                 int top_frq = pq.top().first;
-                char top_val = pq.top().second;
                 pq.pop();
 
                 total += top_frq*curr;
-                cout << top_val << "-" << total << endl;
             }
             curr++;
         }
