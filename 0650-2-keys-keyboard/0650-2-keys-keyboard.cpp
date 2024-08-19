4ef5782123218652ff74b253533297a3c89fa02c
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-08-19 11:34:29 +0530

    Time: 3 ms (69.17%), Space: 7.4 MB (71.95%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
-    int minSteps(int n) {
-        if(n==1) return 0;
-        int c = 0, f = 2;
-        while(n>1){
-            while(n%f==0) {
-                n/=f;
-                c+=f;
-            }
-            f++;
-            
+    bool isprime(int n){
+        if(n==1){
+            return false;
         }
-        return c;
+        if(n==2 || n==3){
+            return true;
+        }
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    int recur(int n){
+        if(n==1){
+            return 0;
+        }
+        if(isprime(n)){
+            return n;
+        }
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                return (recur(i))+recur(n/i);
+            }
+        }
+        return n;
+    }
+    int minSteps(int n) {
+        return recur(n);
     }
 };
