933c66f256df72ca67d390022c7d606b7555ab14
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-09-23 21:19:16 +0530

    Time: 143 ms (47.3%), Space: 98 MB (34.29%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    unordered_map<string, int> um;
+    unordered_map<string, int> memo;
+
+    int solve(string s) {
+        if (s.empty()) return 0;
+
+        if(memo.find(s) != memo.end()) return memo[s];
+
+        int extra = INT_MAX;
+        string tmp = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            tmp += s[i];
+            if (um.find(tmp) != um.end()) extra = min(extra, solve(s.substr(i + 1)));
+        }
+
+        extra = min(extra, 1 + solve(s.substr(1)));
+        return memo[s] = extra;;
+    }
+
+public:
+    int minExtraChar(string s, vector<string>& dict) {
+        for(auto i: dict) um[i]++;
+        return solve(s);
+    }
+};
