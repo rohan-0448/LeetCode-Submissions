85d7f6f26b136eefc9a06d1d2de71af633846e3b
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2024-11-02 00:21:38 +0530

    Time: 23 ms (17.49%), Space: 48.6 MB (14.26%) - LeetHub

---
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    static int maxWidthRamp(vector<int>& nums) {
+        const int n=nums.size();
+        vector<unsigned> numsIdx(n);
+        for(int i=0; i<n; i++) numsIdx[i]=((unsigned)nums[i]<<16)+i;
+
+        sort(numsIdx.begin(), numsIdx.end());
+        int min_i=n, width=0;
+        for(auto x: numsIdx){
+            int i=x&((1<<16)-1);
+            width=max(width, i-min_i);
+            min_i=min(min_i, i);
+        }
+        return width;
+    }
+};
