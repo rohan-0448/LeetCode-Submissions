e0d3362e8dbeb2cad23d5cfd5312614ffe2a12d0
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-16 22:42:15 +0530

    Time: 41 ms (69.96%), Space: 45.3 MB (69.56%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
-    
-    void dp(vector<int> &nums, int index, int target, int &c){
-        if(index == 0) {
-            if(target == nums[index]) c++;
-            return;
-        }
-        if(target == 0){ 
-            c++;
-            return;
-        }
-        if(target < 0) return;
-        
-        dp(nums, index-1, target-nums[index], c);
-        dp(nums, index-1, target, c);
-    }
-    
     int subarraySum(vector<int>& nums, int k) {
-        // int c = 0;
-        // dp(nums, nums.size()-1, k, c);
-        // return c;
-        //Prefix sum approach
-        
         unordered_map<int,int> mp; // prefixsum and frequency map 
         int count = 0 , sum = 0; // initialise counter and sum with 0
-        mp[sum] = 1; // we will put 1 in the map first
+        mp[sum] = 1;
         
         for(auto i : nums){
-            sum += i; //calculate the prefix sum 
-            if(mp.find(sum - k) != mp.end()) count += mp[sum - k]; // we using this case X + (X - k) = k
+            sum += i;
+            if(mp.find(sum - k) != mp.end()) count += mp[sum - k];
             mp[sum]++;
         }
         return count;
