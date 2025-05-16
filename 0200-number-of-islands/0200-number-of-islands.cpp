df04111a68b563a664af0783e45c11a67db2a154
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-16 19:30:22 +0530

    Time: 25 ms (73.38%), Space: 16.1 MB (88.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
 
-    void bfs(int i, int j, vector<vector<char>> &grid, vector<vector<int>> &visited){
-        if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 || visited[i][j] == 1 || grid[i][j] == '0') return;
+    void bfs(int i, int j, vector<vector<char>> &grid){
+        if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 || grid[i][j] == '0') return;
 
-        visited[i][j] = 1;
+        grid[i][j] = '0';
 
-        bfs(i+1, j, grid, visited);
-        bfs(i-1, j, grid, visited);
-        bfs(i, j+1, grid, visited);
-        bfs(i, j-1, grid, visited);
+        bfs(i+1, j, grid);
+        bfs(i-1, j, grid);
+        bfs(i, j+1, grid);
+        bfs(i, j-1, grid);
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int cnt = 0, m = grid.size(), n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int> (n, 0));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == '1' && visited[i][j] == 0){
-                    bfs(i, j, grid, visited);
+                if(grid[i][j] == '1'){
+                    bfs(i, j, grid);
                     cnt++;
                 }
             }
