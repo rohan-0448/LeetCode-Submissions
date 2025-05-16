fbb344093bd56814110e330adcd3b6dce7dedd5b
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-17 00:56:01 +0530

    Time: 0 ms (100%), Space: 16.7 MB (72.34%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int row = grid.size(), col = grid[0].size(), cnt = 0;
         queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> vis(row, vector<int>(col, 0));
 
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j] == 2) {
+        int time = 0, m = grid.size(), n = grid[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 2){
                     q.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                } 
-                if(grid[i][j] == 1) cnt++;
-            }
-        }
-
-        int time = 0;
-        vector<int> dcol = {-1, 0, 1, 0};
-        vector<int> drow = {0, -1, 0, 1};
-
-        while(q.empty() == false){
-            int x = q.front().first.first, y = q.front().first.second;
-            time = q.front().second;
-            q.pop();
-
-            for(int i=0; i<4; i++){
-                int nrow = x+drow[i], ncol = y+dcol[i];
-                if(nrow < row && nrow >=0 && ncol < col && ncol >=0 && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, time+1});
-                    vis[nrow][ncol] = 2;
-                    cnt--;
                 }
             }
         }
 
-        if(!cnt) return time;
-        else return -1;
-        // for(int i=0; i<row; i++) for(int j=0; j<col; j++) if(vis[i][j] != 2 && grid[i][j] == 1) return -1;
-        // return time;
+        
+        vector<int> c = {1, -1, 0 , 0, 0, 0, 1, -1};
+        while(!q.empty()){
+            pair<pair<int, int>, int> top = q.front();
+            q.pop();
+            int x = top.first.first, y = top.first.second, t = top.second;
+            time = max(time, t);
+            
+            for(int i=0; i<4; i++){
+                if(x+c[i]<m && x+c[i]>=0 && y+c[i+4]<n && y+c[i+4]>=0) {
+                    if(grid[x+c[i]][y+c[i+4]] == 1){
+                        grid[x+c[i]][y+c[i+4]] = 2;
+                        q.push({{x+c[i], y+c[i+4]}, t+1});
+                    }
+                }  
+            }
+        }
+        for(auto i: grid) for(auto j: i) if(j == 1) return -1;
+        return time;
     }
 };
