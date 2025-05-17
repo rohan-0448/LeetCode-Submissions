a88f122c6988bbad24294f8fb798d4f0ae3093e0
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-17 08:32:06 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,17 +3,24 @@ public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         int st = 0, ed = n-1;
-
-        for(int i=0; i<=ed; i++){
-            if(nums[i]==0){
-                nums[i] = nums[st];
-                nums[st++] = 0;
+        for(int i=0; i<=ed;){
+            // cout << nums[i] << " " << st << "-" << ed << endl;
+            // if(st > ed) break;
+            if(nums[i] == 0 && st < n){
+                int temp = nums[st];
+                nums[st] = nums[i];
+                nums[i] = temp;
+                st++;
+                i++;
             }
-            else if(nums[i]==2){
-                nums[i] = nums[ed];
-                nums[ed--] = 2;
-                i--;
+            else if(nums[i] == 2 && ed >= 0){
+                int temp2 = nums[ed];
+                nums[ed] = nums[i];
+                nums[i] = temp2;
+                ed--;
+                // i--;
             }
+            else i++;
         }
     }
 };
