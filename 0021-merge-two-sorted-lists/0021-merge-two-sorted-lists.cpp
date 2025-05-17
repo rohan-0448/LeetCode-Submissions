62ea9fa25ec866699a6790d46114d6c450253d65
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-17 11:07:22 +0530

    Time: 0 ms (100%), Space: 20 MB (5%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,31 +12,34 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-        ListNode* temp = new ListNode(0);
-        ListNode* head = temp;
-        while(list1 != NULL && list2 != NULL){
-            if(list1 -> val <= list2 -> val){
-                temp -> next = new ListNode(list1 -> val);
-                list1 = list1 -> next;
-            }
-            else{
-                temp -> next = new ListNode(list2 -> val);
-                list2 = list2 -> next;
-            }
-            temp = temp -> next;
-        }
-        while(list1){
-            temp -> next = new ListNode(list1 -> val);
-            list1 = list1 -> next;
-            temp = temp -> next;
-        }
-        while(list2){
-            temp -> next = new ListNode(list2 -> val);
-            list2 = list2 -> next;
-            temp = temp -> next;
-        }
-        return head -> next;
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
 
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                temp->next = new ListNode(list1->val);
+                temp = temp->next;
+                list1 = list1->next;
+            }else{
+                temp->next = new ListNode(list2->val);
+                temp = temp->next;
+                list2 = list2->next;
+            }
+        }
+
+        while(list1){
+            temp->next = new ListNode(list1->val);
+            temp = temp->next;
+            list1 = list1->next;
+        }
+
+        while(list2){
+            temp->next = new ListNode(list2->val);
+            temp = temp->next;
+            list2 = list2->next;
+        }
+
+        return head->next;
 
 
 
