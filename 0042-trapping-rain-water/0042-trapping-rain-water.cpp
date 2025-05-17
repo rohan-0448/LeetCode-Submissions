3c9b1b1cf72d03c2ba494c82fecc2e19d03a9b19
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-18 00:59:13 +0530

    Time: 0 ms (100%), Space: 26 MB (79.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        // Sliding Window Method
-        int n = height.size();
-        int left = 0, right = n-1, leftmax = 0, rightmax = 0, res = 0;
-        
-        while(left <= right) {
-            if(height[left] <= height[right]){
-                if(height[left] >= leftmax) leftmax = height[left++];
-                else res += leftmax - height[left++];
-            }else{
-                if(height[right] >= rightmax) rightmax = height[right--];
-                else res += rightmax - height[right--];
+    int trap(vector<int>& nums) {
+        int ind = -1, mx = INT_MIN, n = nums.size(), val = INT_MIN, sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] > mx){
+                mx = nums[i];
+                ind = i;
             }
         }
-        return res;
+
+        for(int i = 0; i<ind; i++){
+            val = max(val, min(nums[i], nums[ind]));
+            if(i!=0) sum += (val-nums[i]);
+        }
+        val = INT_MIN;
+        for(int i=n-1; i>ind; i--){
+            val = max(val, min(nums[i], nums[ind]));
+            if(i!=n-1) sum += (val-nums[i]);
+        }
+
+        return sum;
+        // int start = 0, end = 0, sum = 0;
+        // for(int i=1; i<nums.size()-1; i++){
+        //     if(nums[i] > nums[i+1]){
+        //         // while(nums[i] > nums[i+1]) i++;
+        //         start = i;
+        //         i++;
+        //         while(nums[i] > nums[i+1]) i++;
+        //         end = i;
+
+        //         int val = min(nums[start], nums[end]);
+        //         cout << start << "-" << end << "--" << val << endl;
+        //         for(int j=start+1; j<end; j++){
+        //             sum += (val-nums[j]);
+        //             nums[j] = val;
+        //         }
+        //         i--;
+        //     }
+        // }
+        // for(auto i: nums) cout << i << ", ";
+        // cout << endl;
+        // return sum;
     }
 };
