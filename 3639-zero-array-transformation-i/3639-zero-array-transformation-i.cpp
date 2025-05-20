e61f684c06843c50f4c4225772d878e7668a83c7
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-20 21:55:43 +0530

    Time: 134 ms (31.1%), Space: 325.6 MB (11.3%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int sum = 0;
+        // vector<int> presum(n, 0);
+        // for(int i=0; i<n; i++){
+        //     sum += nums[i];
+        //     presum[i] = sum;
+        // }
+
+        vector<int> changes(n+1, 0);
+        for(auto i: queries){
+            int left = i[0];
+            int right = i[1];
+            changes[left]++;
+            changes[right+1]--;
+        }
+
+        for(int i=0; i<n; i++){
+            sum += changes[i];
+            if(sum<nums[i]) return false;
+        }
+        return true;
+        // return (accumulate(nums.begin(), nums.end(), 0) == 0);
+    }
+};
