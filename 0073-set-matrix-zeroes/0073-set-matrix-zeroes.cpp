5c89801b3fa3faf384c2780389930a136882b095
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-21 12:04:35 +0530

    Time: 0 ms (100%), Space: 18.6 MB (52.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,15 +1,69 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) { 
-        ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-        
-        vector<pair<int, int>> coords;
-        for(int row=0; row<matrix.size(); row++) for(int col=0; col<matrix[row].size(); col++) if(matrix[row][col]==0) coords.push_back({row, col});
-        
-        for(auto [row, col]: coords){
-            for(int r2=row, c2=0; c2<matrix[row].size(); c2++) matrix[r2][c2] = 0;
-            for(int r3=0, c3=col; r3<matrix.size(); r3++) matrix[r3][c3] = 0;
+    void setZeroes(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        bool zero = false;
+
+        for(int i=0; i<n; i++){
+            if(mat[i][0] == 0) zero = true;
+            for(int j=1; j<m; j++){
+                if(mat[i][j] == 0) {
+                    mat[0][j] = 0;
+                    mat[i][0] = 0;
+                    // for(int i_=0; i_<mat.size(); i_++) if(mat[i_][j] != 0) mat[i_][j] = -1;
+                    // for(int j_=0; j_<mat[0].size(); j_++) if(mat[i][j_] != 0) mat[i][j_] = -1;
+                }
+            }
         }
-        return;
+
+        // for(auto i: mat){
+        //     for(auto j: i){
+        //         cout << j << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=1; j--){
+                // if(i == 0 && j == 0) continue;
+                if(mat[i][0] == 0 || mat[0][j] == 0){
+                    // for(int i_=1; i_<n; i_++) mat[i_][j] = 0;
+                    // for(int j_=1; j_<m; j_++) mat[i][j_] = 0;
+                    mat[i][j] = 0;
+                }
+            }
+            if(zero){
+                mat[i][0] = 0;
+            }
+        }
+
+        // for(auto i: mat){
+        //     for(auto j: i){
+        //         cout << j << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        // if(mat[0][0] == 0) {
+        //     for(int i_=0; i_<n; i_++) mat[i_][0] = 0;
+        //     for(int j_=0; j_<m; j_++) mat[0][j_] = 0;
+        // }
+
+        // for(int i=0; i<mat.size(); i++){
+        //     if(mat[i][0] == 0 && mat[]){
+        //         for(int j=0; j<mat[i].size(); j++){
+        //             mat[i][j] = 0;
+        //         }
+        //     }
+        // }
+        // for(int j=0; j<mat[0].size(); j++){
+        //     if(mat[0][j] == 0) {
+        //         for(int i=0; i<mat.size(); i++){
+        //             mat[i][j] = 0;
+        //         }
+        //     }
+        // }
     }
 };
