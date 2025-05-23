a8f616b1f411795e1649341b33dc20e88e6cffbf
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-23 15:49:15 +0530

    Time: 7 ms (96.33%), Space: 16.6 MB (72.31%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -6,20 +6,17 @@ public:
             distance[i][i] = 0;  // distance to itself is 0
         }
 
-        // The distance between nodes which are connected temporary distance between them
         for (const auto& edge : edges) {
             int node1 = edge[0], node2 = edge[1], dist = edge[2];
             distance[node1][node2] = dist;
             distance[node2][node1] = dist;
         }
 
-        for (int midle = 0; midle < n; ++midle) {
-            for (int source = 0; source < n; ++source) {
-                for (int destination = 0; destination < n; ++destination) {
-                    if (distance[source][midle] < INT_MAX && distance[midle][destination] < INT_MAX) {
-                        distance[source][destination] = min(
-                            distance[source][destination], distance[source][midle] + distance[midle][destination]
-                        );  // the minimum distance is either current value or new value with path that goes through midle
+        for (int midle = 0; midle < n; midle++) {
+            for (int source = 0; source < n; source++) {
+                for (int destination = 0; destination < n; destination++) {
+                    if(distance[source][midle] < INT_MAX && distance[midle][destination] < INT_MAX) {
+                        distance[source][destination] = min(distance[source][destination], distance[source][midle] + distance[midle][destination]);
                     }
                 }
             }
@@ -28,13 +25,9 @@ public:
         int minimum_number = n;
         int res = -1;
 
-        for (int source = 0; source < n; ++source) {
+        for (int source = 0; source < n; source++) {
             int source_count = 0;
-            for (int destination = 0; destination < n; ++destination) {
-                if (distance[source][destination] <= distanceThreshold) {
-                    ++source_count;
-                }
-            }
+            for (int destination = 0; destination < n; ++destination) if (distance[source][destination] <= distanceThreshold) source_count++;
 
             if (source_count <= minimum_number) {  // as in dijkstra when number equal we choose greater node
                 minimum_number = source_count;
