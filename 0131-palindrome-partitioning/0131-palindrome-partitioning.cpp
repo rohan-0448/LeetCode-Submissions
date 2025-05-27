bc663d1f54aa0186cb1096039c889ac71d7afc35
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-27 10:17:21 +0530

    Time: 137 ms (13.99%), Space: 144.9 MB (12.96%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> temp;
-        func(0, s, temp, ans);
-        return ans;
-    }
-    
-    void func(int index, string s, vector<string> temp, vector<vector<string>> &ans){
-        if(index == s.length()){
-            ans.push_back(temp);
+    void recursive(int index, string s, vector<string> curr, vector<vector<string>> &ans) {
+        if(index == s.length()) {
+            ans.push_back(curr);
             return;
         }
-        for(int i=index; i<s.length(); i++){
-            if(ispalin(s, index, i+1)){
-                temp.push_back(s.substr(index, i+1-index));
-                func(i+1, s, temp, ans);
-                temp.pop_back();
+
+        for(int i=index; i<s.length(); i++) {
+            string sub = s.substr(index, i-index+1);
+            if(check_palin(sub)) {
+                curr.push_back(sub);
+                recursive(i+1, s, curr, ans);
+                curr.pop_back();
             }
         }
     }
-    
-    bool ispalin(string s, int start, int end){
-        string new1 = s.substr(start, end-start);
-        string new2 = new1;
-        reverse(new2.begin(), new2.end());
-        return (new1 == new2);
+
+    bool check_palin(string s) {
+        int i = 0, j = s.length()-1;
+
+        while(i < j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+
+        recursive(0, s, {}, ans);
+
+        return ans;
     }
 };
