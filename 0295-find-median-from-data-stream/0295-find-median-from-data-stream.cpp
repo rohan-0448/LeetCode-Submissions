101b82c827a3524953ff350e239c286bdd6b5679
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-05-28 12:39:17 +0530

    Time: 55 ms (69.03%), Space: 124.9 MB (74.11%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,18 +1,38 @@
 class MedianFinder {
 public:
-    vector<int> nums;
+    priority_queue<int> mx_heap; // max at top -- left
+    priority_queue<int, vector<int>, greater<int>> mn_heap; // min at top -- right
+    // vector<int> nums;
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        auto it = lower_bound(nums.begin(), nums.end(), num);
-        nums.insert(it, num);
+        if(mx_heap.size() == 0 || mx_heap.top() < num) mn_heap.push(num);
+        else mx_heap.push(num);
+
+        if(mn_heap.size() > mx_heap.size()) {
+            int temp = mn_heap.top();
+            mn_heap.pop();
+            mx_heap.push(temp);
+        }
+        if(mx_heap.size() > mn_heap.size()+1) {
+            int temp = mx_heap.top();
+            mx_heap.pop();
+            mn_heap.push(temp);
+        }
+        // auto it = lower_bound(nums.begin(), nums.end(), num);
+        // nums.insert(it, num);
     }
     
     double findMedian() {
-        int n = nums.size();
-        if(n%2==1) return nums[n/2];
-        else return (nums[n/2] + nums[(n-1)/2]) / 2.0;
+        int mx = mx_heap.top(), mn = mn_heap.top();
+        if(mx_heap.size() > mn_heap.size()) return mx;
+        else return (float)(mx + mn) / 2.0;
+        // else return mx+mn;
+
+        // int n = nums.size();
+        // if(n%2==1) return nums[n/2];
+        // else return (nums[n/2] + nums[(n-1)/2]) / 2.0;
     }
 };
