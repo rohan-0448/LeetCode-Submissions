449be5e58ba532874d96cc3759837fc4b16f3d14
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-02 18:43:24 +0530

    Time: 0 ms (100%), Space: 52.4 MB (33.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,7 @@ public:
         int mx = 0, n = nums.size();
         for(int i=0; i<n; i++){
             if(i > mx) return false;
-            if(mx == n-1) return true;
+            if(mx >= n-1) return true;
 
             mx = max(mx, i+nums[i]);
         }
