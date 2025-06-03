b9b3c981637bf99a0d9ed9cc963d56553a8e3d44
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-04 01:43:48 +0530

    Time: 4 ms (1.69%), Space: 11.1 MB (8.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    
-    int check(vector<int> &nums, vector<int> &dp, int index){
-        if(index == 0) return nums[0];
-        if(index <= 0) return 0;
-        
-        if(dp[index] != -1) return dp[index];
-        
-        int include = nums[index] + check(nums, dp, index-2);
-        int not_include = check(nums, dp, index-1);
-            
-        return dp[index] = max(include, not_include);
+    int recur(int ind, vector<int> &nums, vector<int> &dp) {
+        if(ind == -1) return 0;
+        if(ind == 0) return nums[0];
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int take = nums[ind] + recur(ind-2, nums, dp); // take curr and move back 2 spaces;
+        // not take - move back one space;
+        int not_take = recur(ind-1, nums, dp);
+
+        return dp[ind] = max(take, not_take);
     }
-    
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        if(n==0) return 0;
-        vector<int> dp(n, -1), dp2(n, -1);
-        vector<int> a = vector<int>(nums.begin(), nums.end()-1);
-        vector<int> b = vector<int>(nums.begin()+1, nums.end());
-        return max(check(a, dp, n-2), check(b, dp2, n-2));
+        if(n == 1) return nums[0];
+        
+        vector<int> temp1, temp2;
+        for(int i=0; i<n; i++) {
+            if(i != 0) temp1.push_back(nums[i]);
+            if(i != n-1) temp2.push_back(nums[i]);
+        }
+
+        vector<int> dp1(n-1, -1), dp2(n-1, -1);
+        return  max(recur(n-2, temp1, dp1), recur(n-2, temp2, dp2));
     }
 };
