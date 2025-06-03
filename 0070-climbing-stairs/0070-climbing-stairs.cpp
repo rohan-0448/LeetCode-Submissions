d916460c980581ba835586fca1b49e1b51ee9d0e
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-04 01:10:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (57.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,14 +9,24 @@ public:
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
+        // vector<int> dp(n+1, -1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        int prev = 1;
+        int prev2 = 1;
 
-        for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i=2; i<=n; i++) { 
+            int curi = prev + prev2;
+            prev2 = prev;
+            prev = curi;
         }
-        return dp[n];
-        return ways(n, dp);
+
+        return prev;
+
+        // for(int i=2; i<=n; i++) {
+            // dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+        // return ways(n, dp);
     }
 };
