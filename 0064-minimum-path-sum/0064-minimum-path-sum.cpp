2606ae1455193f49b1c7c66c822dd27d3a79e552
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-05 01:30:21 +0530

    Time: 0 ms (100%), Space: 16.3 MB (58.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    
-    // Recursion with Memoization
-    int f(vector<vector<int>> &dp, vector<vector<int>> &grid, int x, int y){
-        if(x == 0 && y == 0) return grid[0][0];
-        if(dp[x][y] != -1) return dp[x][y];
-        
-        int top = INT_MAX, left = INT_MAX;
-        if(x > 0) top = grid[x][y] + f(dp, grid, x-1, y);
-        if(y > 0) left = grid[x][y] + f(dp, grid, x, y-1);
-    
-        return dp[x][y] = min(top, left);
+    int min_cost(int r, int c, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if(r == 0 && c == 0) return grid[0][0];
+        if(dp[r][c] != -1) return dp[r][c];
+            
+        int left = INT_MAX, up = INT_MAX;
+        if(r > 0) up = min_cost(r-1, c, grid, dp);
+        if(c > 0) left = min_cost(r, c-1, grid, dp);
+
+        return dp[r][c] = grid[r][c] + min(up, left);
     }
-    
+
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m,   vector<int> (n, 0));
-        
-        // Tabulation Method
-        for(int x = 0; x < m; x++){
-            for(int y = 0; y < n; y++){
-                int top = grid[x][y], left = grid[x][y];
-                if(x == 0 && y == 0) dp[x][y] = grid[x][y];
-                else{
-                    if(x > 0) top += dp[x-1][y];
-                    else top = INT_MAX;
-                    if(y > 0) left += dp[x][y-1];
-                    else left = INT_MAX;
-                }
-                dp[x][y] = min(top, left);
-            }
-        }
-        return dp[m-1][n-1];
-        return f(dp, grid, m-1, n-1);
+        int r = grid.size(), c = grid[0].size();
+        vector<vector<int>> dp(r, vector<int> (c, -1));
+
+        return min_cost(r-1, c-1, grid, dp);
     }
 };
