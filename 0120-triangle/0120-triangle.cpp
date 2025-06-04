d99aa44a8f92257cbcb3c5e34d47e19fb4058722
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-05 02:03:34 +0530

    Time: 0 ms (100%), Space: 13.3 MB (11.44%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,48 +1,15 @@
 class Solution {
 public:
-    
-    // Recursion with Memoization
-    int f(int i, int j, vector<vector<int>> &triangle, vector<vector<int>> &dp){
-        if(i == triangle.size()-1) return triangle[i][j];
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = triangle[i][j] + f(i+1, j, triangle, dp);
-        int diagonal = triangle[i][j] + f(i+1, j+1, triangle, dp);
-        
-        return dp[i][j] = min(down, diagonal);
+    int min_sum(int x, int y, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+        if(x == triangle.size()-1) return triangle[x][y];
+        if(dp[x][y] != -1) return dp[x][y];
+
+        return dp[x][y] = triangle[x][y] + min(min_sum(x+1, y, triangle, dp), min_sum(x+1, y+1, triangle, dp));
     }
-    
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        
-        // Tabulation Method try-2
-        for(int j = 0; j < n; j++) dp[n-1][j] = triangle[n-1][j];
-
-        for(int i = n-2; i >= 0; i--){
-            for(int j = 0; j <= i; j++){
-                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
-            }
-        }
-        return dp[0][0];
-        
-//         // Tabulation Method
-//         int min_l = INT_MAX;
-//         dp[0][0] = triangle[0][0];
-//         for(int i = 1; i < n; i++){
-//             for(int j = 0; j < i; j++){
-//                 int top = INT_MAX, top_left = INT_MAX;
-//                 if(dp[i-1][j] != -1) top = dp[i-1][j];                
-//                 if(dp[i-1][j-1] != -1) top_left = dp[i-1][j-1];
-                
-//                 dp[i][j] = triangle[i][j];
-//                 if(top != INT_MAX || top_left != INT_MAX) dp[i][j] += min(top, top_left);                
-        
-//                 if(i == n-1) min_l = min(min_l, dp[i][j]);
-//             }
-//         }
-//         return min_l;
-        return f(0, 0, triangle, dp);
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return min_sum(0, 0, triangle, dp);
     }
 };
