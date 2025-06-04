edac407c2fd440fdbb27b5a01161954978bccfe1
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-05 01:05:58 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int count_paths(int x, int y, vector<vector<int>> &dp) {
-        if(x == 0 && y == 0) return 1;
-        if(dp[x][y] != -1) return dp[x][y]; 
+    // int count_paths(int x, int y, vector<vector<int>> &dp) {
+    //     if(x == 0 && y == 0) return 1;
+    //     if(dp[x][y] != -1) return dp[x][y]; 
 
-        int top = 0, left = 0;
-        if(x > 0) top = count_paths(x-1, y, dp);
-        if(y > 0) left = count_paths(x, y-1, dp);
+    //     int top = 0, left = 0;
+    //     if(x > 0) top = count_paths(x-1, y, dp);
+    //     if(y > 0) left = count_paths(x, y-1, dp);
 
-        return dp[x][y] = top + left;
-    }
+    //     return dp[x][y] = top + left;
+    // }
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n, -1));
@@ -24,6 +24,6 @@ public:
 
         return dp[m-1][n-1];
 
-        return count_paths(m-1, n-1, dp);
+        // return count_paths(m-1, n-1, dp);
     }
 };
