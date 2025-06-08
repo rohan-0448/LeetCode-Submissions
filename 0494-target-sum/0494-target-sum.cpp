349592a7d11db8b49ce41a3dc16b79e278da6923
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-08 20:45:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    int find_subsets(int index, int target, vector<int> &nums, vector<vector<int>> &dp) {
+        if(index == 0) {
+            if(nums[0] == target && nums[0] == 0) return 2;
+            else if(nums[0] == target || target == 0) return 1;
+            return 0;
+        }
+
+        if(dp[index][target] != -1) return dp[index][target];
+
+        int not_take = find_subsets(index-1, target, nums, dp);
+        int take = 0;
+        if(nums[index] <= target) take = find_subsets(index-1, target-nums[index], nums, dp);
+
+        return dp[index][target] = take+not_take;
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int total_sum = accumulate(nums.begin() , nums.end() , 0);
-
-        if((total_sum - target) % 2 == 1){ // think why?
-            return 0;
-        }
-        int req_sum = (total_sum - target)/2 + target;
-        if(req_sum < 0){ // think why
-            return 0;
-        }
-
-        vector<int>dp(req_sum + 1 , 0);
-        dp[0] = 1;
-        for(int i : nums){
-            for(int j = req_sum ; j >= i; j--){
-                dp[j] += dp[j - i];
-            }
-        }
-
-        return dp[req_sum];
+        // basically find 2 set whose difference is target. s1 - s2 = target, s1 + s2 = sum, 
+        int sum = accumulate(nums.begin(), nums.end(), 0), n = nums.size();
+        vector<vector<int>> dp(n, vector<int> ((target+sum)/2+1, -1));
+    
+        if((target + sum) % 2 != 0 || abs(target) > sum) return 0;
+        return find_subsets(n-1, (target+sum) / 2, nums, dp);
     }
 };
