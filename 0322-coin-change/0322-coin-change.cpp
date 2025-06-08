f56b451c8cf6ad06eaabc8e1221348ef59cbd193
rohan-0448 <125554303+rohan-0448@users.noreply.github.com>
2025-06-08 17:58:04 +0530

    Time: 44 ms (39.1%), Space: 49.3 MB (28.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,64 +1,27 @@
 class Solution {
 public:
+    int count(int ind, int amount, vector<int> &coins, vector<vector<int>>& dp) {
+        if(ind == 0) {
+            if(amount % coins[0] == 0) return amount / coins[0];
+            else return 1e9;
+        }
+        if(amount == 0) return 0;
 
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
-        // for(auto i: dp) cout << i << " ";
-        // cout << endl;
-        for(int coin : coins) {
-            for(int i = coin; i <= amount; ++i) dp[i] = min(dp[i], dp[i - coin] + 1);
-            // for(auto i: dp) cout << i << " ";
-            // cout << endl;
-        } 
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+
+        int not_take = count(ind-1, amount, coins, dp);
+        int take = 1e9; // initialise with big value cause we have to do min later
+        if(amount >= coins[ind]) take = 1 + count(ind, amount-coins[ind], coins, dp);
+
+        return dp[ind][amount] = min(take, not_take);
     }
 
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(coins.size(), vector<int> (amount+1, -1));
+        int ans = count(n-1, amount, coins, dp);
 
-// void check(vector<int>& coins, int tar, int ind, int curr, long long& minn, int c) {
-//         if (curr > tar) return;
-//         if (ind == coins.size()) {
-//             if (curr == tar) minn = min(minn, (long long)c);
-//             return;
-//         }
-//         if (curr == tar) {
-//             minn = min(minn, (long long)c);
-//             return;
-//         }
-
-//         check(coins, tar, ind + 1, curr, minn, c);
-//         if(coins[ind] != INT_MAX && curr + coins[ind] <= tar) check(coins, tar, ind, curr + coins[ind], minn, c + 1);
-//     }
-
-//     int coinChange(vector<int>& coins, int amount) {
-//         long long minn = LLONG_MAX;
-//         check(coins, amount, 0, 0, minn, 0);
-
-//         if (minn == LLONG_MAX) return -1;
-//         return (int)minn;
-//     }
-
-    // void check(vector<int>& coins, int tar, int ind, int curr, int& minn, int c){
-    //     cout << curr << " " << ind << " " << c << endl;
-    //     if(curr > tar) return;
-    //     if(ind == coins.size()) {
-    //         if(curr == tar) minn = min(minn, c);
-    //         return;
-    //     }
-    //     if(curr == tar) {
-    //         minn = min(minn, c);
-    //         return;
-    //     }
-
-    //     check(coins, tar, ind+1, curr, minn, c);
-    //     if(curr+coins[ind] <= tar) check(coins, tar, ind, curr+coins[ind], minn, c+1);
-    // }
-
-    // int coinChange(vector<int>& coins, int amount) {
-    //     int minn = INT_MAX;
-    //     check(coins, amount, 0, 0, minn, 0);
-
-    //     if(minn == INT_MAX) return -1;
-    //     return minn;
-    // }
+        if(ans >= 1e9) return -1;
+        else return ans;
+    }
 };
